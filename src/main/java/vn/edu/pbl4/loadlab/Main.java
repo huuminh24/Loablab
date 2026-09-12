79f0efc61efd4b23b2b8fce93e1b6adde91ef74c
Phan Hữu Minh <102240096@sv1.dut.udn.vn>
2026-09-12 13:10:44 +0700

    Thêm main

---
+++ src/main/java/vn/edu/pbl4/loadlab/Main.java
@@ -0,0 +1,8 @@
+package vn.edu.pbl4.loadlab;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("LOADLAB - BUOI 4");
+        System.out.println("Cho pull request cua Hoang va Luc de tich hop.");
+    }
+}
