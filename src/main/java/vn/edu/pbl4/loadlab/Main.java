72645ce8b559ed84abd19915f646732d32d25401
Phan Hữu Minh <102240096@sv1.dut.udn.vn>
2026-09-12 15:18:37 +0700

    feat: integrate first HTTP GET vertical slice

--- src/main/java/vn/edu/pbl4/loadlab/Main.java
+++ src/main/java/vn/edu/pbl4/loadlab/Main.java
@@ -1,8 +1,18 @@
 package vn.edu.pbl4.loadlab;
 
+import vn.edu.pbl4.loadlab.http.HttpRequestSender;
+import vn.edu.pbl4.loadlab.metrics.RequestResult;
+import vn.edu.pbl4.loadlab.testserver.LocalTestServer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("LOADLAB - BUOI 4");
-        System.out.println("Cho pull request cua Hoang va Luc de tich hop.");
+    public static void main(String[] args) throws Exception {
+        LocalTestServer server = new LocalTestServer(8080);
+        server.start();
+
+        RequestResult result = new HttpRequestSender()
+                .sendGet("http://localhost:8080/ok", 3000);
+        System.out.println(result);
+
+        server.stop();
     }
 }
